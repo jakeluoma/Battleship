7858f0b3bd664c162d765d0904b5d11206ede33b
Nikhil Prabhu <nikhill.prabhu@gmail.com>
2019-11-28 23:33:42 -0700

    Adds ProgramCoordinator to coordinate between screens and appropriate functions; adds supporting dicitonaries, more canvases, moves input parsing methods from view to InputParser

--- Program.py
+++ Program.py
@@ -1,5 +1,5 @@
 from Statistics import *
-from view import View, login_canvas
+from view import View, login_canvas, start_menu_canvas, MenuOption, exit_canvas
 
 
 class Program:
@@ -8,9 +8,7 @@ class Program:
         self.ai_types = [0, 1]
         self.view = View()
 
-    def login(self, user_name: str):
-        self.view.set_canvas(login_canvas)
-        self.view.display_canvas()
+    def login(self):
         user_name = self.view.get_username()
         user = UserProfile(user_name)
         if user_name in self.known_users:
@@ -24,11 +22,43 @@ class Program:
         print(Statistics.lifetime_stats_to_string(user))
         print(Statistics.most_recent_game_stats_to_string(user))
 
+    def start_menu(self):
+        pass
+
     def start_game(self):
         pass
 
+    def exit(self):
+        exit()
+
+
+class ProgramAndViewCoordinator:
+    option_canvas_map = {
+        MenuOption.LOGIN: login_canvas,
+        MenuOption.EXIT: exit_canvas,
+        MenuOption.STARTMENU: start_menu_canvas,
+        # MenuOption.SHOWSTATS: stats_canvas
+    }
+
+    option_program_method_map = {
+        MenuOption.STARTMENU: 'start_menu',
+        MenuOption.LOGIN: 'login',
+        MenuOption.SHOWSTATS: 'show_user_stats',
+        MenuOption.EXIT: 'exit',
+    }
+
+    def __init__(self, program: Program, view: View):
+        self.program = program
+        self.view = view
+
+    def run_screen(self, menu_option: MenuOption):
+        self.view.update_display(self.option_canvas_map[menu_option])
+        getattr(self.program, self.option_program_method_map[menu_option])()
+        next_menu = self.view.get_next_view()
+        self.run_screen(next_menu)
+
 
 if __name__ == '__main__':
-    p = Program()
-    puser = p.login("nikhil")
-    p.show_user_stats(puser)
+    p, v = Program(), View()
+    pc = ProgramAndViewCoordinator(p, v)
+    pc.run_screen(MenuOption.STARTMENU)
--- view.py
+++ view.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 from typing import Tuple, Optional
 
 from Coordinate import Coordinate
@@ -7,8 +8,15 @@ from board import Direction
 center_format = "{0:^100}\n"
 
 
-class Canvas:
+class MenuOption(Enum):
+    STARTMENU = 0
+    LOGIN = 1
+    EXIT = 2
+    STARTGAME = 3
+    SHOWSTATS = 4
 
+
+class Canvas:
     def __init__(self):
         self.view_width = 100
 
@@ -17,7 +25,6 @@ class Canvas:
 
 
 class LoginCanvas(Canvas):
-
     def __init__(self):
         super().__init__()
         self.display_string = center_format.format("xxxxxx Login xxxxxx") + \
@@ -27,35 +34,43 @@ class LoginCanvas(Canvas):
         print(self.display_string)
 
 
-class View:
+class MainMenuCanvas(Canvas):
     def __init__(self):
-        self.canvas = None
+        super().__init__()
+        self.display_string = center_format.format("xxxxxx Main Menu xxxxxx") + \
+            center_format.format("1. Play new game") + \
+            center_format.format("2. Show User stats") + \
+            center_format.format("3. Exit") + \
+            center_format.format("Please select an option by typing one of the numbers above")
 
-    def set_canvas(self, canvas):
-        self.canvas = canvas
+    def paint(self):
+        print(self.display_string)
 
-    def clear_screen(self):
-        os.system("cls")
 
-    def display_canvas(self):
-        self.clear_screen()
-        self.canvas.paint()
+class StartMenuCanvas(Canvas):
+    def __init__(self):
+        super().__init__()
+        self.display_string = center_format.format("xxxxxx Start Menu xxxxxx") + \
+            center_format.format("l: Login") + \
+            center_format.format("x: Exit") + \
+            center_format.format("Please select an option by typing one of the numbers above")
 
-    def get_username(self):
-        inp = input()
-        return inp
+    def paint(self):
+        print(self.display_string)
 
-    def get_coordinate(self) -> Coordinate:
-        inp = input()
-        x, y = self.parse_coordinate(inp)
-        return Coordinate(x, y)
+class ExitCanvas(Canvas):
+    def __init__(self):
+        super().__init__()
+        self.display_string = center_format.format("xxxxxx Exit Screen xxxxxx") + \
+            center_format.format("You have exited the game. Goodbye!")
 
-    def get_direction(self) -> Direction:
-        inp = input()
-        dir = self.parse_direction(inp)
-        return dir
+    def paint(self):
+        print(self.display_string)
 
-    def parse_coordinate(self, inp) -> Optional[Tuple[int, int]]:
+
+class InputParser:
+    @staticmethod
+    def parse_coordinate(inp) -> Optional[Tuple[int, int]]:
         x, y = None, None
         if len(inp) == 2:
             x, y = inp[0], inp[1]
@@ -72,20 +87,91 @@ class View:
         except Exception:
             raise Exception("Invalid coordinates")
 
-    def parse_direction(self, inp) -> Direction:
-        if any(inp.startswith(k) for k in ["down", "go down", "below", "bottom", "s"]):
+    @staticmethod
+    def parse_direction(inp) -> Direction:
+        if any(inp.lower().startswith(k) for k in ["down", "go down", "below", "bottom", "s"]):
             return Direction.down
-        elif any(inp.startswith(k) for k in ["up", "go up", "above", "top", "w"]):
+        elif any(inp.lower().startswith(k) for k in ["up", "go up", "above", "top", "w"]):
             return Direction.up
-        elif any(inp.startswith(k) for k in ["left", "go left", "a"]):
+        elif any(inp.lower().startswith(k) for k in ["left", "go left", "a"]):
             return Direction.left
-        elif any(inp.startswith(k) for k in ["right", "go right", "d"]):
+        elif any(inp.lower().startswith(k) for k in ["right", "go right", "d"]):
             return Direction.right
 
         raise Exception("Invalid Direction")
 
+    @staticmethod
+    def parse_next_view(inp) -> MenuOption:
+        if any(inp.lower().startswith(k) for k in ["l", "login"]):
+            return MenuOption.LOGIN
+        elif any(inp.lower().startswith(k) for k in ["e", "x", "exit"]):
+            return MenuOption.EXIT
+        elif any(inp.lower().startswith(k) for k in ["g", "start", "game"]):
+            return MenuOption.STARTGAME
+        elif any(inp.lower().startswith(k) for k in ["s", "stats", "show stats"]):
+            return MenuOption.SHOWSTATS
+
+        raise Exception("Invalid Option")
+
+
+class View:
+    def __init__(self):
+        self.menu_option = None
+        self.canvas = None
+
+    def set_canvas(self, canvas: Canvas):
+        self.canvas = canvas
+
+    def set_menu_option(self, menu_option: MenuOption):
+        self.menu_option = menu_option
+
+    def clear_screen(self):
+        os.system("cls")
+
+    def update_display(self, canvas):
+        self.clear_screen()
+        self.set_menu_option(canvas_option_map[canvas])
+        canvas.paint()
+
+    def get_username(self):
+        inp = input()
+        return inp
+
+    def get_coordinate(self) -> Coordinate:
+        inp = input()
+        x, y = InputParser.parse_coordinate(inp)
+        return Coordinate(x, y)
+
+    def get_direction(self) -> Direction:
+        inp = input()
+        return InputParser.parse_direction(inp)
+
+    def get_next_view(self) -> MenuOption:
+        inp = input()
+        opt = InputParser.parse_next_view(inp)
+
+        if opt not in valid_screen_transitions[self.menu_option]:
+            raise Exception("You cannot use this option in this screen")
+
+        return opt
+
 
 login_canvas = LoginCanvas()
+start_menu_canvas = StartMenuCanvas()
+exit_canvas = ExitCanvas()
+
+canvas_option_map = {
+    login_canvas: MenuOption.LOGIN,
+    exit_canvas: MenuOption.EXIT,
+    start_menu_canvas: MenuOption.STARTMENU,
+    # stats_canvas: MenuOption.SHOWSTATS
+}
+
+valid_screen_transitions = {
+    MenuOption.STARTMENU: [MenuOption.LOGIN, MenuOption.EXIT],
+}
+
+# option_
 
 if __name__ == "__main__":
     # l = LoginCanvas()
