c3c467fadf5df4afa4ca699f965b736fd5e971db
Jake Luoma <luoma.jake@gmail.com>
2019-12-04 16:09:46 -0700

    new test making sure pickle works with lower level classes

--- Game_test.py
+++ Game_test.py
@@ -3,6 +3,9 @@ import pytest
 from game import Game, GameMode
 from player import UserProfile
 
+from board import Board
+import pickle
+
 # will fail until we can actually get a game set up by getting user input
 
 """
@@ -16,4 +19,17 @@ def test_game_save_and_load():
     loaded_game = Game.load_saved_game()
     assert game == loaded_game
 
+"""
+
+"""
+def test_trying_pickle():
+    dimension = 10
+    board = Board(dimension)
+    file_name = "test.p"
+
+    pickle.dump(board, open(file_name, "wb"))
+
+    loaded_board: Board = pickle.load(open(file_name, "rb"))
+
+    assert loaded_board.get_dimension() == board.get_dimension()
 """
