651bafe1850a63139091f45cc9879ddbec27b37b
Jake Luoma <luoma.jake@gmail.com>
2019-12-08 21:40:09 -0700

    test files go in tests folder

--- Coordinate_test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from Coordinate import Coordinate
-
-def test_coordinate_init():
-    coordinate = Coordinate(1, 1)
-    assert coordinate.get_row_and_column() == (1, 1)
---
+++ tests/Coordinate_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+# importing from parent directory: https://gist.github.com/JungeAlexander/6ce0a5213f3af56d7369
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from Coordinate import Coordinate
+
+def test_coordinate_init():
+    coordinate = Coordinate(1, 1)
+    assert coordinate.get_row_and_column() == (1, 1)
